5b28ff84dc07416c643f1ad88a3e3bde13cd5daf
E <evcarone@gmail.com>
2019-12-09 08:47:07 -0800

    added additional user prompts

--- app.js
+++ app.js
@@ -4,22 +4,33 @@ const util = require("util");
 const axios = require("axios");
 const writeFileAsync = util.promisify(fs.writeFile);
 
+function promptRole() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "employeeRole",
+            message: "What is the role of the employee?",
+            choices: ["Manager", "Engineer", "Intern"]
+        }
+    ]
+    )}
 
-function promptUser() {
-    return inquirer.prompt([{
+function promptManager() {
+    return inquirer.prompt([
+        {
             type: "input",
             name: "team",
             message: "What is the team name?"
         },
         {
             type: "input",
-            name: "name",
-            message: "What is the name of the employee?"
+            name: "role",
+            message: "What is the role of the employee?"
         },
         {
             type: "input",
-            name: "role",
-            message: "What is the role of the employee?"
+            name: "name",
+            message: "What is the name of the employee?"
         },
         {
             type: "number",
@@ -31,21 +42,28 @@ function promptUser() {
             name: "email",
             message: "What is the email of the employee?"
         },
-    ]);
-}
-
-promptUser()
+        {
+            type: "input",
+            name: "office",
+            message: "What is the office number of the manager?"
+        }
+    ]
+    )}
+promptRole()
+.then(function(){
+    promptManager()
     .then(function (answer) {
         const teamName = answer.team
         const employeeName = answer.name
         const employeeRole = answer.role
         const employeeID = answer.id
         const employeeEmail = answer.email
+        const employeeOffice = answer.office
 
-        console.log(teamName, employeeName, employeeRole, employeeID, employeeEmail)
+        // console.log(teamName, employeeName, employeeRole, employeeID, employeeEmail, employeeOffice)
 
-        createTeamProfile(teamName)
-        createManager(employeeName, employeeRole, employeeID, employeeEmail)
+        // createTeamProfile(teamName)
+        createManager(employeeName, employeeRole, employeeID, employeeEmail, employeeOffice, teamName)
 
 
     })
@@ -56,34 +74,40 @@ promptUser()
         console.log(err)
     });
 
-function createTeamProfile(name) {
+})
+
+
+
+// function createTeamProfile(name) {
+//     const templateFile = fs.readFileSync('./templates/main.html', {
+//         encoding: 'utf8'
+//     })
+//     let n = name
+
+//     let tempFile = templateFile.replace('{{ team }}', n)
+//     tempFile = tempFile.replace('{{ name }}', "EVELYN")
+
+//     writeFileAsync("./output/index.html", tempFile)
+// }
+
+function createManager(empN, empR, empId, empE, empO, teamN) {
     const templateFile = fs.readFileSync('./templates/main.html', {
-        encoding: 'utf8'
-    })
-    let n = name
-
-    let tempFile = templateFile.replace('{{ team }}', n)
-    tempFile = tempFile.replace('{{ name }}', "EVELYN")
-
-    writeFileAsync("./output/index.html", tempFile)
-}
-
-function createManager(empN, empR, empId, empE) {
-    const templateFile2 = fs.readFileSync('./templates/main.html', {
         encoding: 'utf8'
     })
     let tmpN = empN
     let tmpR = empR
     let tmpId = empId
     let tmpE = empE
+    let tmpO = empO
+    let tmpT = teamN
 
-    console.log("value of tempN is", tmpN)
+    let tempFile = templateFile.replace('{{ name }}', tmpN)
+    tempFile = tempFile.replace('{{ role }}', tmpR)
+    tempFile = tempFile.replace('{{ id }}', tmpId)
+    tempFile = tempFile.replace('{{ email }}', tmpE)
+    tempFile = tempFile.replace('{{ email }}', tmpE)
+    tempFile = tempFile.replace('{{ officeNumber }}', tmpO)
+    tempFile = tempFile.replace('{{ team }}', tmpT)
 
-    let tempFile2 = templateFile2.replace('{{ name }}', "EVELYN")
-    tempFile2 = tempFile2.replace('{{ role }}', tmpR)
-    tempFile2 = tempFile2.replace('{{ id }}', tmpId)
-    tempFile2 = tempFile2.replace('{{ email }}', tmpE)
-    tempFile2 = tempFile2.replace('{{ email }}', tmpE)
-
-    writeFileAsync("./output/indexNEW.html", tempFile2)
+    writeFileAsync("./output/index.html", tempFile)
 }
